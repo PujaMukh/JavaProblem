f5d8d7e473ff38957e16f3a098f9a3bc78c02f82
Puja Mukherjee <pujam836@gmail.com>
2022-03-11 16:33:41 -0800

    1st commit. Find the square root of a number if it is perfect square. If not a perfect square, return -1

---
+++ src/java_simpleprograms/FindSqRoot.java
@@ -0,0 +1,23 @@
+package java_simpleprograms;
+//find square root of a number if it is perfect square. Else return -1 if not perfect square
+
+import java.lang.*;
+import java.util.*;
+
+public class FindSqRoot {
+        static int solve(int A) { //return type here is integer
+            for (int i=1; i<=A; i++) { //logic
+                if (i*i==A) {
+                    return i; //if perfect square, return the i
+                }
+            }
+            return -1;  //if not, return -1 after loop ends
+        }
+        public static void main(String args[]) {
+            Scanner sc=new Scanner(System.in);
+            int A= sc.nextInt();
+            System.out.println(solve(A));
+        }
+    }
+
+
