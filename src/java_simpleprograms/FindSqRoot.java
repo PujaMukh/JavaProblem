c89b716b96f58d71d603873e4935252a40d5c8e6
Puja Mukherjee <pujam836@gmail.com>
2022-03-11 18:51:20 -0800

    changed the i max value

--- src/java_simpleprograms/FindSqRoot.java
+++ src/java_simpleprograms/FindSqRoot.java
@@ -1,12 +1,12 @@
 package java_simpleprograms;
 //find square root of a number if it is perfect square. Else return -1 if not perfect square
-
+//max value of A can be 10^8
 import java.lang.*;
 import java.util.*;
 
 public class FindSqRoot {
         static int solve(int A) { //return type here is integer
-            for (int i=1; i<=A; i++) { //logic
+            for (int i=1; i<=10000; i++) { //logic
                 if (i*i==A) {
                     return i; //if perfect square, return the i
                 }
