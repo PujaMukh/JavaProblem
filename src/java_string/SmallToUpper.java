fbce7cb0b5d7845cb864bd48ba063d8d182ef3b4
Puja Mukherjee <pujam836@gmail.com>
2022-03-14 13:47:09 -0700

    1st commit. Convert a smaller case string to its capital format

---
+++ src/java_string/SmallToUpper.java
@@ -0,0 +1,37 @@
+package java_string;
+//given a string in small, convert it to it's upper format
+//eg. input-puja, output-PUJA
+
+import java.lang.*;
+import java.util.*;
+
+public class SmallToUpper {
+    static String smallToUpperString(String A) { //return type is string
+        //now convert it to char array and store it in char array named ch
+        char[]ch=A.toCharArray();
+
+        int N=ch.length; //take the length of array ch
+
+         char[] B=new char[N]; //new array to store the upper character result
+
+        for (int i=0; i<N; i++) {
+            B[i]=upperChar(ch[i]);
+        }
+
+        String output=new String(); //create new output string named output
+        output=String.valueOf(B); //convert the char array B to string named output
+        return output;
+
+    }
+    static char upperChar(char a) { //pass a char named a
+        int d=(int) a- 32; //convert the char to int and store it in int d
+        char b=(char) d; //convert the int d with upper value in char b
+        return b;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        System.out.print(smallToUpperString(A));
+    }
+}
