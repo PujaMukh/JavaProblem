af8cfc72dd5ec4f2ccb13e7cc7cf4c21fd70d5b6
Puja Mukherjee <pujam836@gmail.com>
2022-03-17 15:11:44 -0700

    1st commit. Sum of number A and its reverse using string(memory)

---
+++ src/java_simpleprograms/ReverseSum.java
@@ -0,0 +1,37 @@
+package java_simpleprograms;
+
+//Given an integer, find the sum of the number and its reverse.
+//Example Input:
+//N = 536
+//Example Output:
+//1171
+//Explanation:
+//536 + 635 = 1171
+
+
+import java.lang.*;
+import java.util.*;
+public class ReverseSum {
+    static int revSum(int A) {
+
+        int temp=A;
+        //to reverse 1st
+        String S="";
+        while (A>0) {
+            int d=A%10;
+            A=A/10;
+            S=S+d;
+        }
+        //now string S contains the reversed value
+        //time to convert the string to int
+        int rev=Integer.valueOf(S);
+        int result=rev+temp;
+        return result;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        System.out.print(revSum(A));
+    }
+}
