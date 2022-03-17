c732c9cabdf8d91183d3aac40b850eb1c334a50b
Puja Mukherjee <pujam836@gmail.com>
2022-03-17 12:59:16 -0700

    1st commit. Print fibonacci pattern. 1st I printed the 1st 2 rows and then I did 2 more for loops for remaining rows

---
+++ src/java_pattern/FibonnaciPattern.java
@@ -0,0 +1,39 @@
+package java_pattern;
+//Take N (number of rows), print the following pattern (for N = 4)
+// 0
+// 1 1
+// 2 3 5
+// 8 13 21 34
+
+import java.lang.*;
+import java.util.*;
+
+public class FibonnaciPattern {
+ public static void main(String args[]) {
+     Scanner sc=new Scanner(System.in);
+     int N=sc.nextInt(); //N input is number of rows
+
+ //1st I am printing 1st 2 rows
+     for (int i=1; i<=2; i++) {
+             if (i==1) {
+                 System.out.println("0");
+             }
+             else if (i==2) {
+                 System.out.println("1"+" "+"1");
+             }
+         }
+
+     //now printing remaining rows based on logic
+     int sum1=1;
+     int sum2=1;
+     for (int i=3; i<=N; i++) {
+         for (int j=1; j<=i; j++) {
+             int temp=sum1;
+             sum1=sum1+sum2;
+             System.out.print(sum1+" ");
+             sum2=temp;
+         }
+         System.out.println();
+     }
+ }
+}
