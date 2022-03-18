5178e72f45fc385f9d4dd1983811185e26a4f99c
Puja Mukherjee <pujam836@gmail.com>
2022-03-18 16:42:34 -0700

    1st commit

---
+++ src/java_very_simpleproblems/CheckIfprime.java
@@ -0,0 +1,26 @@
+package java_very_simpleproblems;
+//Given an integer A, you have to tell whether it is a prime number or not.
+//A prime number is a natural number greater than 1 which is divisible only by 1 and itself.
+
+import java.lang.*;
+import java.util.*;
+public class CheckIfprime {
+    //1st find factors. If factors=2, the number is prime, else not
+    static int primeNumber(int A) {
+        int c=0;
+        for (int i=1; i<=A; i++) {
+            if (A%i==0) {
+                c++;
+            }
+        }
+        if (c==2) {
+            return 1;
+        }
+       return 0;
+    }
+    public static void main(String args[])   {
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        System.out.print(primeNumber(A));
+    }
+}
