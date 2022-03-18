69509033eaea3cb50ef9da000289ce28e12b2c62
Puja Mukherjee <pujam836@gmail.com>
2022-03-17 23:12:17 -0700

    1st commit

---
+++ src/java_very_simpleproblems/MinOf2.java
@@ -0,0 +1,19 @@
+package java_very_simpleproblems;
+import java.lang.*;
+import java.util.*;
+public class MinOf2 {
+        public static void main(String[] args) {
+
+            Scanner puja=new Scanner(System.in);
+            int A=puja.nextInt();
+            int B=puja.nextInt();
+
+            if (A<=B) {
+                System.out.println(A);
+            }
+            else {
+                System.out.println(B);
+            }
+        }
+    }
+
