c4f8ea941b8cff21048f5cc8a5636c0ac400b5b9
Puja Mukherjee <pujam836@gmail.com>
2022-03-18 12:53:46 -0700

    1st commit

---
+++ src/java_very_simpleproblems/CheckIfPalindrome.java
@@ -0,0 +1,29 @@
+package java_very_simpleproblems;
+//Given an integer A, determine whether it is palindromic or not.
+//A palindrome integer is an integer X for which reverse(X) = X
+// where reverse(X) is X with its digits reversed. For e.g., reverse(123) = 321.
+
+import java.lang.*;
+import java.util.*;
+public class CheckIfPalindrome {
+    static int palindromeNumber(int A) {
+        //I will use formula num=num*10+d to reverse the number (base 10)
+        int temp=A; //preserving the A in temp for later
+        int rev=0;
+        while(temp>0) {
+            int d=temp%10;
+            rev=rev*10+d;
+            temp=temp/10;
+        }
+
+        if (rev==A) {
+            return 1;
+        }
+        return 0;
+    }
+    public static void main(String args[])   {
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        System.out.print(palindromeNumber(A));
+    }
+}
