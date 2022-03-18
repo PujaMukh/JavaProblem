3b4ed037f603ae148ed969d66ad4d461ef58e365
Puja Mukherjee <pujam836@gmail.com>
2022-03-17 23:06:23 -0700

    1st commit

---
+++ src/java_very_simpleproblems/MaxOf2.java
@@ -0,0 +1,20 @@
+package java_very_simpleproblems;
+import java.lang.*;
+import java.util.*;
+
+public class MaxOf2 {
+        public static void main(String[] args) {
+
+            Scanner puja=new Scanner(System.in);
+            int A=puja.nextInt();
+            int B=puja.nextInt();
+
+            if (A>=B) {
+                System.out.println(A);
+            }
+            else {
+                System.out.println(B);
+            }
+        }
+    }
+
