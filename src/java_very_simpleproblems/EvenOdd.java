3531c11df160a90f13931b11fd379a25d3c45b2c
Puja Mukherjee <pujam836@gmail.com>
2022-03-17 23:11:16 -0700

    1st commit

---
+++ src/java_very_simpleproblems/EvenOdd.java
@@ -0,0 +1,16 @@
+package java_very_simpleproblems;
+import java.lang.*;
+import java.util.*;
+public class EvenOdd {
+        public static void main(String[] args) {
+            Scanner puja=new Scanner(System.in);
+            int N=puja.nextInt();
+            if (N % 2 !=0) {
+                System.out.println("1");
+            }
+            else {
+                System.out.println("0");
+            }
+        }
+    }
+
