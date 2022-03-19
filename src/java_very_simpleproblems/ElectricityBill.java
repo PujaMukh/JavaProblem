4a7ea67d1402d7d4aa77c33f1aff09bfcf19b9b0
PujaMukh <99165658+PujaMukh@users.noreply.github.com>
2022-03-18 20:20:08 -0700

    Update ElectricityBill.java

--- src/java_very_simpleproblems/ElectricityBill.java
+++ src/java_very_simpleproblems/ElectricityBill.java
@@ -1,18 +1,6 @@
 package java_very_simpleproblems;
 //Given an integer A denoting the amount of units of electricity consumed,
 // you have to calculate the electricity bill (in Rs.) with the help of the below charge conditions:
-//For first 50 units Rs. 0.50/unit
-//For next 100 units Rs. 0.75/unit
-//For next 100 units Rs. 1.20/unit
-//For unit above 250 Rs. 1.50/unit
-//An additional surcharge of 20% is added to the bill.
-//NOTE: As the electricity bill can have any real value (floating point),
-// you have to tell the floor value of the bill.
-//Floor value of a floating point is the closest integer less than or equal to that value.
-// For eg, Floor value of 2.91 is 2.
-//Input Format
-//First line of the input contains a single integer A.
-//Output Format
 //Return an integer denoting the floor value of the Electricity bill.
 
 import java.lang.*;
