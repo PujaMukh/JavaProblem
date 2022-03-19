28044718d2170d2d673db075fa9fb3cd29368c4d
Puja Mukherjee <pujam836@gmail.com>
2022-03-18 23:36:07 -0700

    changed A from 65 to 'A' which is the same

--- src/java_pattern/CharacterPattern.java
+++ src/java_pattern/CharacterPattern.java
@@ -11,7 +11,7 @@ public class CharacterPattern {
     static void charPrint(int N) {
         for (int i=1; i<=N; i++) { //outer row
             for (int j=1; j<=i; j++) {
-                char ch=(char)(65+(i-1)); //ASCII of A is 65
+                char ch=(char)('A'+(i-1));
                 System.out.print(ch+" ");
             }
             System.out.println();
