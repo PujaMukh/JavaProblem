f8e00ae90a77ad4544b2244a69c5ae39632c7b4c
PujaMukh <99165658+PujaMukh@users.noreply.github.com>
2022-03-18 20:23:08 -0700

    Update BankAccount.java

--- src/java_very_simpleproblems/BankAccount.java
+++ src/java_very_simpleproblems/BankAccount.java
@@ -1,26 +1,5 @@
 package java_very_simpleproblems;
-//You are given a Bank account having N amount and you are asked to perfrom ADD(credit)
-// and SUBTRACT(debit) operations.
-//After each operation print the amount left in the Bank account. If the debit amount is greater
-// than current balance print "Insufficient Funds"(without quotes)
-// and the operation is skipped.
-//Input Format
-//First line contains a single integer N denoting the balance in bank account.
-//Second line contains a single integer M denoting the number of operations.
-//Each of next M lines contains two space separated integers Type and Amount(X).
-//If Type == 1, Perform ADD operation.
-//If Type == 2, Perform SUBTRACT operation.//Output Format
-//Print Amount in the bank balance after each operation on a new line.
-//Example Input
-//1000
-//3
-//1 500
-//2 1400
-//2 500
-//Example Output
-//1500
-//100
-//Insufficient Funds
+//add or subtract in a bank account balance and return the remaining
 
 import java.lang.*;
 import java.util.*;
