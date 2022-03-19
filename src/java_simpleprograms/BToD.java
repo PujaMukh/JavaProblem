61dfb1f2b29cca7ac1d15224ac5e65a36672836e
Puja Mukherjee <pujam836@gmail.com>
2022-03-18 23:33:40 -0700

    changed 1 comment

--- src/java_simpleprograms/BToD.java
+++ src/java_simpleprograms/BToD.java
@@ -9,7 +9,7 @@ public class BToD {
     static int binaryToDecimal(String A) {
         int num=0;
         for (int i=0; i<A.length(); i++) {
-            num=num*2+A.charAt(i)-'0'; //here -'0' to preserve the value
+            num=num*2+A.charAt(i)-'0'; //here -'0' convert the char to int
             //or else it is converting to ascii of the number at charAti
         }
         return num;
