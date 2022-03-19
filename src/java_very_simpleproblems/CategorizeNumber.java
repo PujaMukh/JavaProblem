e7cb0431ccb2b7af833c702a03e4b08c19566d5b
PujaMukh <99165658+PujaMukh@users.noreply.github.com>
2022-03-18 20:27:44 -0700

    Update CategorizeNumber.java

--- src/java_very_simpleproblems/CategorizeNumber.java
+++ src/java_very_simpleproblems/CategorizeNumber.java
@@ -1,8 +1,8 @@
 package java_very_simpleproblems;
 import java.lang.*;
 import java.util.*;
-//Write a program to input a number(A) from user and print 1 if it is positive,
-// -1 if it is negative, 0 if it's neither positive nor negative.
+//Write a program to input a number from user and print 1 if it is positive,
+// -1 if it is negative, else 0
 public class CategorizeNumber {
     public static void main(String args[]) {
             Scanner puja=new Scanner(System.in);
