14d3e089b08f206da7995bbe92749b26860c9bda
PujaMukh <99165658+PujaMukh@users.noreply.github.com>
2022-03-18 21:09:34 -0700

    Update SeriesPrinting.java

--- src/java_simpleprograms/SeriesPrinting.java
+++ src/java_simpleprograms/SeriesPrinting.java
@@ -1,14 +1,6 @@
 package java_simpleprograms;
 
-//Take the following as input.
-//A number (N1) A number (N2) Write a function which prints first N1 terms of the series
-// 3n + 2 which are not multiples of N2.
-//Sample Input:
-//10 4
-//Sample Output:
-//5 11 14 17 23 26 29 35 38 41
-//Explanation:
-//The output will have N1 terms which are not divisible by N2.
+//Write a function which prints first N1 terms of the series 3n + 2 which are not multiples of N2.
 
 import java.lang.*;
 import java.util.*;
