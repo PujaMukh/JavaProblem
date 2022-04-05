4340c8b6b28922bdd87dc91354843072089edfc6
Puja Mukherjee <pujam836@gmail.com>
2022-04-05 12:27:50 -0700

    1st commit. Finding if the given array can be broken into subarrays of even length where 1st and last element is even

---
+++ src/carry_forward/EvenSubarrays.java
@@ -0,0 +1,29 @@
+package carry_forward;
+//check whether it is possible to divide given array into one or more subarrays where:
+//length of all subarrays even and 1st and last element of all subarrays even
+
+import java.lang.*;
+import java.util.*;
+public class EvenSubarrays {
+    static int subArrays(int[]A) {
+        int N=A.length; //size of array
+
+        if (N%2!=0) {
+            return 0;
+        }
+        else if (A[0]%2==0 && A[N-1]%2==0) {
+            return 1;
+        }
+        return 0;
+    }
+    public static void main(String args[])   {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt(); //size of array
+        int[]A=new int[N];
+        for (int i=0; i<N; i++) {
+            A[i]=sc.nextInt(); //input for all array elements
+        }
+        System.out.print(subArrays(A));
+    }
+}
+//SC is O(1)
