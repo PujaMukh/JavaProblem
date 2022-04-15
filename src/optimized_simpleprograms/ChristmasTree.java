7108eba6386f86c7ecd59aff2e46d06c1578f768
Puja Mukherjee <pujam836@gmail.com>
2022-04-15 10:07:41 -0700

    calculate sum and minsum only when we have all values in left and right

--- src/optimized_simpleprograms/ChristmasTree.java
+++ src/optimized_simpleprograms/ChristmasTree.java
@@ -17,24 +17,29 @@ public class ChristmasTree {
 
         for (int j=1; j<N-1; j++) { //middle element
             int sum=0;
-            int left=Integer.MAX_VALUE;
             int right=Integer.MAX_VALUE;
+            int left=Integer.MAX_VALUE;
 
             //leftside traverse
             for (int i=0; i<=j-1; i++) {
                 if (A.get(i)<A.get(j)) {
+
                     left=Math.min(left,B.get(i)); //if more than 1 smaller in left, find the one with minimum cost
                 }
             }
             //right traverse
             for (int k=j+1; k<N; k++) {
                 if(A.get(k)>A.get(j)) {
+
                    right=Math.min(right, B.get(k));
                 }
             }
 
-            sum=left+right+B.get(j);
-            minSum=Math.min(sum, minSum);
+            if (left!=Integer.MAX_VALUE && right!=Integer.MAX_VALUE) { //if there is no left or right,
+                //do not calculate the sum and minsum
+                sum=left+right+B.get(j);
+                minSum=Math.min(sum, minSum);
+            }
 
         }
         return minSum;
