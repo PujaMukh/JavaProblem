532a34988c6e9716ea3c4ed98d4a35b13b815a84
Puja Mukherjee <pujam836@gmail.com>
2022-04-15 09:17:53 -0700

    finding the min cost to buy 3 christmas trees in ascending order in O(N^2) time and O(1) space. I started mid from 1 to n-2 as 1st and last element can never be a mid. calculate min cost in leftside if more than 1 element less than A[j] and same on right. Then add the j value and left and right. Continue considering all mid values. Then find min of all these values at the end

---
+++ src/optimized_simpleprograms/ChristmasTree.java
@@ -0,0 +1,60 @@
+package optimized_simpleprograms;
+//given an array A with height of christmas tree and an array B with cost of them,
+//find the min cost to chose 3 trees in ascending order
+import java.lang.*;
+import java.util.*;
+
+
+public class ChristmasTree {
+    static int christmasTree (ArrayList<Integer>A, ArrayList<Integer>B) {
+        //logic:fix a mid j, left i and right k
+        //for every mid find the min cost on left side if more than 1 on leftside
+        //same for right side
+        //mid will be ++
+        //lastly min of all the ans we got
+        int N=A.size();
+        int minSum=Integer.MAX_VALUE;
+
+        for (int j=1; j<N-1; j++) { //middle element
+            int sum=0;
+            int left=Integer.MAX_VALUE;
+            int right=Integer.MAX_VALUE;
+
+            //leftside traverse
+            for (int i=0; i<=j-1; i++) {
+                if (A.get(i)<A.get(j)) {
+                    left=Math.min(left,B.get(i)); //if more than 1 smaller in left, find the one with minimum cost
+                }
+            }
+            //right traverse
+            for (int k=j+1; k<N; k++) {
+                if(A.get(k)>A.get(j)) {
+                   right=Math.min(right, B.get(k));
+                }
+            }
+
+            sum=left+right+B.get(j);
+            minSum=Math.min(sum, minSum);
+
+        }
+        return minSum;
+    }
+
+    public static void main(String args[])   {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt(); //take input for size of array
+
+        ArrayList<Integer>A=new ArrayList<Integer>(); //declare input array
+        for (int i=0; i<N; i++) { //take input for array elements
+           A.add(sc.nextInt());
+        }
+
+        ArrayList<Integer>B=new ArrayList<Integer>(); //declare input array
+        for (int i=0; i<N; i++) { //take input for array elements
+            B.add(sc.nextInt());
+        }
+        System.out.println(christmasTree(A,B));
+    }
+
+}
+//TC is O(N^2), SC is O(1)
