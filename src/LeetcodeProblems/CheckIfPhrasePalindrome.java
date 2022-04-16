af63b0c8364105a94783822ca34b00004196fd94
Puja Mukherjee <pujam836@gmail.com>
2022-04-16 11:32:32 -0700

    checking if phrase is a palindrome

---
+++ src/LeetcodeProblems/CheckIfPhrasePalindrome.java
@@ -0,0 +1,50 @@
+package LeetcodeProblems;
+import java.lang.*;
+import java.util.*;
+public class CheckIfPhrasePalindrome {
+        static boolean isPalindrome(String s) {
+            int N=s.length(); //length of string
+            StringBuilder sb=new StringBuilder();
+
+            //hint: 1st put all smaller characters and all capital to smaller chars in a //stringbuilder. Then traverse from back of stringbuilder and check if it is same.
+            //dont forget to preserve the value of stringbuilder
+
+            for (int i=0; i<N; i++) {
+                if (s.charAt(i)>='a' && s.charAt(i)<='z') {
+                    sb.append(s.charAt(i));
+
+                }
+                else if (s.charAt(i)>='A' && s.charAt(i)<='Z') {
+                    int convertToInteger=(int)s.charAt(i)+32;
+                    char c=(char) convertToInteger;
+                    sb.append(c);
+
+                }
+            }
+
+            //String input=sb.toString(); //convert it to string
+
+
+            StringBuilder output=new StringBuilder(); //to store output
+
+            int M=sb.length(); //length of the stringbuilder
+
+            for (int i=M-1; i>=0; i--) {
+                output.append(sb.charAt(i));
+
+            }
+            if (output.toString().equals(sb.toString())) {
+                return true;
+            }
+            return false;
+
+        }
+
+    public static void main(String args[])   {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine(); //take input for string
+
+        System.out.println(isPalindrome(s));
+    }
+    }
+//TC is O(N), SC is O(1)
