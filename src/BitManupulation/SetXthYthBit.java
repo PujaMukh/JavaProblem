a7a33626ec4a387703d313ebfcc265282b2a63cd
Puja Mukherjee <pujam836@gmail.com>
2022-04-20 11:57:07 -0700

    set the xth and yth bit as 1 given all 0 numbers and return the result (without using if else)

---
+++ src/BitManupulation/SetXthYthBit.java
@@ -0,0 +1,23 @@
+package BitManupulation;
+//given x and y, set xth and yth bit in value 0
+
+import java.lang.*;
+import java.util.*;
+public class SetXthYthBit {
+    static int setBits(int x, int y) {
+        return ((1<<x) | (1<<y));
+    }
+
+    public static void main(String args[])   {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt(); //take input for size of array
+        int y=sc.nextInt();
+        System.out.println(setBits(x,y));
+    }
+
+}
+//TC is O(1), SC is O(1)
+
+//another way to do this is by using if else
+//if x==y, then return either 1<<x or 1<<y
+//else return 1<<x+1<<y (2^x+2^y)
