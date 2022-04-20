0e61b2cdf6d56b9d2c441338421d2f1603c92816
Puja Mukherjee <pujam836@gmail.com>
2022-04-20 12:20:47 -0700

    set ith bit in n and return the result in O(1) time and no if else

---
+++ src/BitManupulation/SetIthBitInN.java
@@ -0,0 +1,19 @@
+package BitManupulation;
+//given n and i, set ith bit in n
+
+import java.lang.*;
+import java.util.*;
+public class SetIthBitInN {
+    static int setBit(int n, int i) {
+        return (n | (1<<i));
+    }
+
+    public static void main(String args[])   {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i=sc.nextInt();
+        System.out.println(setBit(n, i));
+    }
+
+}
+//TC is O(1), SC is O(1)
