567dcfafed66d572dc3cd287267d27dd0f73c710
Puja Mukherjee <pujam836@gmail.com>
2022-04-25 20:44:53 -0700

    concatenate 3 integers in O(1) time

---
+++ src/ModularArithmatic/Concatenate3Numbers.java
@@ -0,0 +1,25 @@
+package ModularArithmatic;
+//concatenate 3 numbers
+//eg 10, 20, 30 will be 102030
+
+import java.lang.*;
+import java.util.*;
+public class Concatenate3Numbers {
+    static int concatenate(int A, int B, int C) {
+        //find min, max and mid 1st
+        int min=Math.min(A, Math.min(B,C));
+        int max=Math.max(A, Math.max(B, C));
+        int mid=A^B^C^max^min; //it will find mid as the otehr 4 will be xeroed out
+
+        return min*10000+mid*100+max;
+    }
+    public static void main(String args[])   {
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+        int C=sc.nextInt();
+        System.out.println(concatenate(A,B,C));
+
+
+    }
+}
