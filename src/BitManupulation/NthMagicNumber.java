629fd6cc62cb0a77f1d67920acc7426cd5034fcd
Puja Mukherjee <pujam836@gmail.com>
2022-04-27 14:35:17 -0700

    find the nth magic number of 5. I did it using right shift bit manipulation. Hint is to write the binary of 1st few A's

---
+++ src/BitManupulation/NthMagicNumber.java
@@ -0,0 +1,34 @@
+package BitManupulation;
+//find the nth magic number
+//magic number is a number that can be expressed in powers of 5 or sum of unique powers of 5
+//based on the answers, I observed that it is:
+//5^position of binary of 1 in A
+//eg. 1=001=5^1, 3=011=5^1+5^2
+
+import java.lang.*;
+import java.util.*;
+
+public class NthMagicNumber {
+    static int findMagicNumber(int A) {
+        //using rightshift operator
+        int pow=1;
+        int ans=0;
+
+        while(A>0) {
+
+            pow=pow*5;
+            if ((A&1)==1) {
+                ans=ans+pow;
+            }
+            A=A>>1;
+        }
+        return ans;
+    }
+
+    public static void main(String args[])   {
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        System.out.println(findMagicNumber(A));
+    }
+}
+//TC is O(log n), SC is O(1)
