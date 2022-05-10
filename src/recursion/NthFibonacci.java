1c41dbced1b17f81c5e2090a29d4ca6e6ce29719
Puja Mukherjee <pujam836@gmail.com>
2022-05-09 19:48:01 -0700

    find the nth fibonacci number using recursion

---
+++ src/recursion/NthFibonacci.java
@@ -0,0 +1,17 @@
+package recursion;
+import java.lang.*;
+import java.util.*;
+//calculate Nth fibonacci number using recursion
+public class NthFibonacci {
+    static int findFib(int N) {
+        if ((N==1)|| (N==2)) {
+            return 1;
+        }
+        return findFib(N-1)+findFib(N-2);
+    }
+    public static void main(String args[])   {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt(); //take input for size of array
+        System.out.println(findFib(N));
+    }
+}
