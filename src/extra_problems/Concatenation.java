aba0cec96d9ef6ef0bd08c5935244058cf376ced
Puja Mukherjee <pujam836@gmail.com>
2022-05-29 13:13:32 -0700

    concatenate and divide by B

--- src/extra_problems/Concatenation.java
+++ src/extra_problems/Concatenation.java
@@ -1,5 +1,6 @@
 package extra_problems;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 
@@ -7,27 +8,23 @@ import java.util.Scanner;
 //concatenation is divisible by B (consider all pairs except when i==j)
 //eg except 00, 11, 22 etc
 public class Concatenation {
-    public static int concat(String[]A, int B) {
-        HashSet<String>hs=new HashSet<>();
-        for (int i=0; i<A.length; i++) {
-            for (int j=0; j<A.length; j++) {
+    public static int concat(ArrayList<String>A, int B) {
+        ArrayList<String>res=new ArrayList<>();
+        for (int i=0; i<A.size(); i++) {
+            for (int j=0; j<A.size(); j++) {
                 if (i!=j) {
-                    String S=A[i]+A[j];
-                    if (S.charAt(0)!='0') {
-                        hs.add(S);
-                    }
+                    res.add(A.get(i)+A.get(j));
                 }
             }
         }
         int count=0;
-        for (String str:hs) { //str is like i(loop by value)
-            if (checkIfDivisible(str,B)==true) {
+        for (int i=0; i<res.size(); i++) {
+            if (checkIfDivisible(res.get(i),B)==true) {
                 count++;
 
             }
-
-
         }
+
         return count;
 
     }
@@ -36,10 +33,11 @@ public class Concatenation {
         int i=0;
         int num=0;
         int mod=0;
-        StringBuilder sb=new StringBuilder();
+
         while(i<A.length()) {
-            sb.append(A.charAt(i));
-            num=Integer.parseInt(sb.toString());
+            String S=Integer.toString(mod)+A.charAt(i);
+
+            num=Integer.parseInt(S);
             mod=num%B;
             i++;
         }
@@ -51,9 +49,10 @@ public class Concatenation {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int N=sc.nextInt();
-        String[]S=new String[N];
-        for (int i=0; i<S.length; i++) {
-            S[i]=sc.next();
+       ArrayList<String>S=new ArrayList<>();
+        for (int i=0; i<N; i++) {
+            S.add(sc.next());
+    //        System.out.println(S.get(i));
         }
         int B=sc.nextInt();
         System.out.println(concat(S,B));
